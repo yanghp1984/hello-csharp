dcb59980d88797b93fc2026ecc17b1e99f38ffa3
yanghp1984 <yanghp1984@outlook.com>
2017-08-22 00:41:06 +0800

    第3次提交，创建HelloJava.cs

---
+++ HelloWorld/HelloJava.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorld
+{
+    class HelloJava
+    {
+        static void main(String[] args)
+        {
+            Console.WriteLine("Hello Java！");
+        }
+    }
+}
