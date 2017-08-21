594c3a10797fa7069bd4262dc2008be4f64ae91f
yanghp1984 <yanghp1984@outlook.com>
2017-08-22 00:39:56 +0800

    第2次提交，创建HelloCSharp.cs

---
+++ HelloWorld/HelloCSharp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorld
+{
+    class HelloCSharp
+    {
+        static void main(String[] args)
+        {
+            Console.WriteLine("Hello C# !");
+        }
+    }
+}
